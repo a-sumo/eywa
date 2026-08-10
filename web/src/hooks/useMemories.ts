722e18c7b822664b68de8a3d591a803f992f4836
Armand Sumo <armandsumo@gmail.com>
2026-08-10 21:06:54 +0200

    Archive Eywa and point to Swarmline (#2)

--- web/src/hooks/useMemories.ts
+++
@@ -1,3 +0,0 @@
-// Re-export from realtime hooks for backwards compatibility
-export { useRealtimeMemories as useMemories } from "./useRealtimeMemories";
-export { useRealtimeAgents as useAgents } from "./useRealtimeMemories";
